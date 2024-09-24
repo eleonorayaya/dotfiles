748c97adb4b6933ec3b030b7e91ce327584d9598
Burke Livingston <burkel24@gmail.com>
2024-09-24 03:15:51 -0700

    nvim improve comment shortcut

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -7,7 +7,7 @@ return {
 	config = function()
 		-- import comment plugin safely
 		local comment = require("Comment")
-
+		local api = require("Comment.api")
 		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
 
 		-- enable comment
@@ -15,5 +15,14 @@ return {
 			-- for commenting tsx, jsx, svelte, html files
 			pre_hook = ts_context_commentstring.create_pre_hook(),
 		})
+
+		vim.keymap.set("n", "<C-_>", api.toggle.linewise.current)
+
+		local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+
+		vim.keymap.set("x", "<C-_>", function()
+			vim.api.nvim_feedkeys(esc, "nx", false)
+			api.toggle.linewise(vim.fn.visualmode())
+		end)
 	end,
 }
