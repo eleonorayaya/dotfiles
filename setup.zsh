c683ff48238b6ef64d41b529bb626f73cb55c9c9
Burke Livingston <burkel24@gmail.com>
2024-10-08 09:57:09 -0700

    tmux and vim fun

--- setup.zsh
+++ setup.zsh
@@ -9,5 +9,9 @@ mkdir -p ~/.config
 ln -s ~/.dotfiles/nvim ~/.config/nvim
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
+# Install tmux sessionizer
+# Ensure that ~/.cargo/bin is in your PATH (add to .env file)
+cargo install tmux-sessionizer
+
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # Run C-b I to install plugins
