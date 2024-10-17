927554a4c81d622c47415f81babf27750eba5449
Burke Livingston <burkel24@gmail.com>
2024-10-16 20:53:22 -0700

    feat(nvim): add per-branch sessions and harpoon list

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,11 +1,40 @@
 return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
+	opts = {
+		settings = {},
+	},
 	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
 	config = function()
 		local conf = require("telescope.config").values
 		local telescope_state = require("telescope.actions.state")
 		local telescope_finders = require("telescope.finders")
+
+		local is_git_dir = function()
+			local out = vim.fn.systemlist("git rev-parse --is-inside-work-tree")
+
+			if vim.v.shell_error ~= 0 then
+				print(string.format("git failed with: %s", table.concat(out, "\n")))
+				return false
+			end
+
+			return out[1] == "true"
+		end
+
+		local get_current_branch = function()
+			if not is_git_dir() then
+				return ""
+			end
+
+			local out = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")
+
+			if vim.v.shell_error ~= 0 then
+				print(string.format("git failed with: %s", table.concat(out, "\n")))
+				return ""
+			end
+
+			return out[1]
+		end
 
 		local harpoon_get_paths = function(files)
 			local paths = {}
@@ -16,14 +45,26 @@ return {
 		end
 
 		local function harpoon_make_finder(paths)
+			put(paths)
 			return telescope_finders.new_table({ results = paths })
 		end
 
 		local harpoon = require("harpoon")
-		harpoon:setup({})
+		harpoon.setup({
+			settings = {
+				save_on_toggle = true,
+				sync_on_ui_close = true,
+			},
+		})
 
 		vim.keymap.set("n", "<leader>a", function()
-			harpoon:list():add()
+			local n = vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf())
+			if string.match(n, "NvimTree") then
+				return
+			end
+
+			local c = get_current_branch()
+			require("harpoon"):list(c):add()
 		end)
 
 		local function toggle_telescope(harpoon_files)
@@ -43,8 +84,9 @@ return {
 							local selected_entry = telescope_state.get_selected_entry()
 							local current_picker = telescope_state.get_current_picker(prompt_buffer_number)
 
-							table.remove(harpoon:list().items, selected_entry.index)
-							current_picker:refresh(harpoon_make_finder(harpoon_get_paths(harpoon:list())))
+							local c = get_current_branch()
+							table.remove(harpoon:list(c).items, selected_entry.index)
+							current_picker:refresh(harpoon_make_finder(harpoon_get_paths(harpoon:list(c))))
 						end)
 
 						return true
@@ -54,7 +96,8 @@ return {
 		end
 
 		vim.keymap.set("n", "<C-o>", function()
-			toggle_telescope(harpoon:list())
+			local c = get_current_branch()
+			toggle_telescope(harpoon:list(c))
 		end, { desc = "Open harpoon window" })
 	end,
 }
--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -42,10 +42,21 @@ return {
 				custom = { ".DS_Store" },
 			},
 			git = {
-				ignore = false,
+				enable = false,
 			},
 		})
 
+		vim.api.nvim_create_autocmd({ "BufEnter" }, {
+			pattern = "NvimTree*",
+			callback = function()
+				local api = require("nvim-tree.api")
+				local view = require("nvim-tree.view")
+
+				if not view.is_visible() then
+					api.tree.open()
+				end
+			end,
+		})
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
 
