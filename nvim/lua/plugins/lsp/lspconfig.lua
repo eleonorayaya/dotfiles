af260c63ea25fdaa2c8f740942dbc50d1ba4d7f9
Burke Livingston <burkel24@gmail.com>
2024-10-19 10:58:13 -0700

    feat(nvim): add auto import fixing for go files on write

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -19,6 +19,21 @@ return {
 
 		local keymap = vim.keymap -- for conciseness
 
+		local function organize_imports()
+			local params = vim.lsp.util.make_range_params()
+			params.context = { only = { "source.organizeImports" } }
+			local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 1000)
+			for _, res in pairs(result or {}) do
+				for _, r in pairs(res.result or {}) do
+					if r.edit then
+						vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
+					else
+						vim.lsp.buf.execute_command(r.command)
+					end
+				end
+			end
+		end
+
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 			callback = function(ev)
@@ -65,6 +80,13 @@ return {
 
 				opts.desc = "Restart LSP"
 				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+			end,
+		})
+
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*.go",
+			callback = function()
+				organize_imports()
 			end,
 		})
 
