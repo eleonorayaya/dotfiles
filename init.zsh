220b212a8805c62a19d214658803df399eb175f0
Burke Livingston <burke.livingston@gusto.com>
2025-07-16 11:51:39 -0700

    posh

--- init.zsh
+++ init.zsh
@@ -1,9 +1,12 @@
-. ~/.dotfiles/omzsh.zsh
+# . ~/.dotfiles/omzsh.zsh
 
 . ~/.dotfiles/alias.zsh
 . ~/.dotfiles/env.zsh
 . ~/.dotfiles/shared_env.zsh
 . ~/.dotfiles/functions.zsh
 . ~/.dotfiles/git-autocomplete.zsh
+
+source $(brew --prefix)/opt/zsh-vi-mode/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+eval "$(oh-my-posh init zsh --config ~/.dotfiles/terminal/ohmyposh.toml)"
 
 fastfetch
--- omzsh.zsh
+++
@@ -1,2 +0,0 @@
-export ZSH="$HOME/.oh-my-zsh"
-plugins=(git nvm yarn vscode zsh-autosuggestions)
