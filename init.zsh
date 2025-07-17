769a635690b8074d2be1317331787c590e414c84
Burke Livingston <burke.livingston@gusto.com>
2025-07-17 11:33:03 -0700

    clean up terminal stuff

--- init.zsh
+++ init.zsh
@@ -1,10 +1,6 @@
-. ~/.dotfiles/terminal/shared_env.zsh
+. ~/.dotfiles/shared_env.zsh
 . ~/.dotfiles/env.zsh
 
-. ~/.dotfiles/terminal/alias.zsh
-. ~/.dotfiles/terminal/functions.zsh
-. ~/.dotfiles/terminal/git-autocomplete.zsh
-
-. ~/.dotfiles/terminal/posh.zsh
+. ~/.dotfiles/terminal/init.zsh
 
 fastfetch
--- terminal/posh.zsh
+++
@@ -1,3 +0,0 @@
-source $(brew --prefix)/opt/zsh-vi-mode/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
-
-eval "$(oh-my-posh init zsh --config ~/.dotfiles/terminal/ohmyposh.toml)"
