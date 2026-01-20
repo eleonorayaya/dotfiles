91a78c5e080ae9037d41492f98cb30908cfdcec2
Eleonora Tennyson <eleonora@tennyson.io>
2026-01-20 14:55:08 -0800

    wip

--- apps/nvim/contents/lua/plugins/comment.lua
+++ apps/nvim/contents/lua/plugins/comment.lua
@@ -3,5 +3,15 @@ return {
     "nvim-mini/mini.comment",
     event = "VeryLazy",
     version = "*",
+    config = function()
+      require("mini.comment").setup({
+        mappings = {
+          comment = '',
+          comment_line = '<C-/>',
+          comment_visual = '<C-/>',
+          textobject = '',
+        },
+      });
+    end,
   },
 }
--- apps/nvim/contents/lua/plugins/devicons.lua
+++ apps/nvim/contents/lua/plugins/devicons.lua
@@ -1,5 +1,10 @@
 return {
   {
     "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("nvim-web-devicons").setup({
+        color_icons = false;
+      })
+    end
   },
 }
